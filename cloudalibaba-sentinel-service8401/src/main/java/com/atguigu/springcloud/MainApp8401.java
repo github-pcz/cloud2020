b744292532056d1a9a8626fab815ee0ad8c28456
chengzhe.pan@supwisdom.com <Pancz1112>
2022-06-29 10:26:25 +0800

    add:新增sentinel等相关代码

---
+++ cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/MainApp8401.java
@@ -0,0 +1,17 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author pcz
+ * @create 2022-06-19 15:32
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class MainApp8401 {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApp8401.class, args);
+    }
+}
---
+++ cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/controller/FlowLimitController.java
@@ -0,0 +1,23 @@
+package com.atguigu.springcloud.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author pcz
+ * @create 2022-06-19 15:39
+ */
+@RestController
+public class FlowLimitController {
+
+    @GetMapping("/testA")
+    public String testA(){
+        return "------testA";
+    }
+
+    @GetMapping("/testB")
+    public String testB(){
+        return "------testB";
+    }
+
+}
